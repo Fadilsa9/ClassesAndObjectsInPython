ccfbfb1d34f01f4e4056c0f8f4ecbabc00cee215
ahmed <kgahmed90@gmail.com>
2024-07-01 21:02:07 +0400

    QuestionsAnswers

---
+++ test.py
@@ -0,0 +1,111 @@
+# c = 'good'
+# def myfun():
+#     c = 'awesome'
+#     print('yes it is ' + c)
+    
+# myfun() 
+
+# print(c)
+
+# x = 1j
+# print(type(x)) #<class 'complex'>
+
+# z = 77777j
+# print(z)
+# print(type(z)) 
+
+# import random
+# print(random.randrange(90, 9000)) 
+
+# R = '''fffffffffffffffffff
+# fffffffffffffffffff
+# fffffffffffffffff'''
+# print(R) 
+
+# Z = """mmmmmmmmmmmmmmmmm
+# mmmmmmmmmmmmmmmmmmmm
+# mmmmmmmmmmmmmmmmmm"""
+# print(Z) 
+
+# h = 'assalmu alekum'
+# print(h[2])   
+
+# for z in "orange":
+#     print(z) 
+#     print(len(z)) 
+    
+# G = 'im ahmed'
+# print(len(G)) 
+
+# i = 30
+# g = f'my salary is {i} thousand'  
+# print(g) 
+# print("my salary is " + str(i) + " `thousand") 
+
+
+# cars = 92
+# num = f"the numbers of cars is {cars:.3f}" 
+# print(num) 
+# # print(f'the numbers of cars is {cars}') 
+
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+# calc = f"add 4 on 6 {4 + 6}"
+# print(calc) 
+
+# txt = 'ssssss\rgggggggg'
+# print(txt) 
+
+###################################
+# class myclass():
+#     def __len__(self):
+#         return 0
+# myobj = myclass()
+# print(bool(myobj)) 
+###################################
+
+# a = 19
+# b = 2
+# print(a // b) 
+
+# list = [11, 22, 33]
+# for b in list:
+#     print(b)
+
+# x = range(6)
+# for n in x:
+#     print(n) 
+    
+# x = range(3, 7)
+# for y in x:
+#     print(y) 
+
+# x = range(0, 9, 2)
+# for y in x:
+#     print(y) 
+
+# FRUITS = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# # newlist = [x for x in FRUITS if "a" in x]
+# # newlist = [x.upper() for x in FRUITS]
+# # newlist = ["hello" for x in FRUITS] 
+# # newlist = ['good' for x in FRUITS]
+# newlist = [x.upper() for x in FRUITS]
+# anotherList = [x.lower() for x in FRUITS] 
+# print(newlist) 
+# print(anotherList) 
+
+
+### List
+# 1.
+myList = ['a', 'b', 'c', 'd', 'f']
+print(myList) 
+
+# 2. 
+myList.append('G') 
+print(myList) 
+
+myList.insert(2, 'SSS')
+print(myList) 
